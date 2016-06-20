91b4a50a7e556bbe3f655b3e6e67b955a722874c
Zarbuz <nicolas.perrier2@numericable.fr>
2016-06-20 12:55:56 +0200

    Add batch file

---
+++ devtool_dungeon_blueprint_to_QB.bat
@@ -0,0 +1,13 @@
+@echo off
+set DESTDIR=%~dp1
+set TROVEDIR=%~dp0
+
+setlocal EnableDelayedExpansion
+FOR /R "%TROVEDIR%extraced\blueprints" %%f IN (*.blueprint) DO ( 
+  set SRC="%%f"
+  set DEST=%%~nf
+  set DESTFN="%TROVEDIR%qbexport\!DEST!.qb"
+  echo !DESTFN!
+  CALL "%TROVEDIR%devtool.bat" copyblueprint -generatemaps 1 !SRC! !DESTFN!
+)
+pause
